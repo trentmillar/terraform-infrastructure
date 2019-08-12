c2deba9b4ad508e8fdccd35ade404560db5ff6d5
trent <trent.millar@gmail.com>
2019-08-12 15:34:22 -0600

    finished private subnets

--- infr/variables.tf
+++ infr/variables.tf
@@ -1,10 +1,33 @@
 variable "region" {
-  default = "us-west-2"
-  description = "Default AWS Region"
+  default       = "us-west-2"
+  description   = "Default AWS Region"
 }
 
 variable "vpc_cidr" {
-  default = "192.168.0.0/16"
-  description = "VPC CIDR Block"
+  default       = "192.168.0.0/16"
+  description   = "VPC CIDR Block"
 }
 
+variable "public_subnet_1_cidr" {
+  description   = "Public Subnet 1 CIDR"
+}
+
+variable "public_subnet_2_cidr" {
+  description   = "Public Subnet 2 CIDR"
+}
+
+variable "public_subnet_3_cidr" {
+  description   = "Public Subnet 3 CIDR"
+}
+
+variable "private_subnet_1_cidr" {
+  description   = "Private Subnet 1 CIDR"
+}
+
+variable "private_subnet_2_cidr" {
+  description   = "Private Subnet 2 CIDR"
+}
+
+variable "private_subnet_3_cidr" {
+  description   = "Private Subnet 3 CIDR"
+}
--- infr/vpc.tf
+++ infr/vpc.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "testing-vpc" {
   }
 }
 
-// Begin - define subnets
+// Begin - Public Subnets
 resource "aws_subnet" "public-subnet-1" {
   cidr_block        = "${var.public_subnet_1_cidr}"
   vpc_id            = "${aws_vpc.testing-vpc.id}"
@@ -46,4 +46,36 @@ resource "aws_subnet" "public-subnet-3" {
       Name = "Public-Subnet-3"
   }
 }
-// End - define subnets
+// End - Public Subnets
+
+// Begin - Private Subnets
+resource "aws_subnet" "private-subnet-1" {
+  cidr_block        = "${var.private_subnet_1_cidr}"
+  vpc_id            = "${aws_vpc.testing-vpc.id}"
+  availability_zone = "us-west-2a"
+
+  tags {
+      Name = "Private-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  cidr_block        = "${var.private_subnet_2_cidr}"
+  vpc_id            = "${aws_vpc.testing-vpc.id}"
+  availability_zone = "us-west-2b"
+
+  tags {
+      Name = "Private-Subnet-2"
+  }
+}
+
+resource "aws_subnet" "private-subnet-3" {
+  cidr_block        = "${var.private_subnet_3_cidr}"
+  vpc_id            = "${aws_vpc.testing-vpc.id}"
+  availability_zone = "us-west-2c"
+
+  tags {
+      Name = "Private-Subnet-3"
+  }
+}
+// End - Private Subnets
