0ccd17c5fdb23ecd05156e1178a4ced5539676f1
trent <trent.millar@gmail.com>
2019-08-13 17:03:16 -0600

    aws launch config including startup script

--- instances/instances.tf
+++ instances/instances.tf
@@ -147,3 +147,27 @@ data "aws_ami" "launch_configuration_ami" {
         values = ["amazon"]
     }
 }
+
+resource "aws_launch_configuration" "ec2_private_launch_configuation" {
+  image_id                    = "${data.aws_ami.launch_configuration_ami.id}"
+  instance_type               = "${var.ec2_instance_type}"
+  key_name                    = "${var.keypair_name}"
+  associate_public_ip_address = false
+  iam_instance_profile        = "${aws_iam_instance_profile.ec2_instance_profile.name}"
+  security_groups             = ["${aws_security_group.ec2_private_security_group.id}"]
+
+  user_data = <<EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install apache2
+    export INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+    echo "<html><body><h1>Running $INSTANCE_ID</h1></body></html>" > /var/www/html/index.html
+  EOF
+}
+
+resource "aws_launch_configuration" "ec2_public_launch_configuration" {
+    image_id = "${data.aws_ami.launch_configuration_ami.id}"
+    instance_type = "${var.ec2_instance_type}"
+    key_name = "${var.keypair_name}"
+    associate_public_ip_address = true
+}
--- instances/variables.tf
+++ instances/variables.tf
@@ -10,4 +10,12 @@ variable "remote_state_key" {
   description = "Key for state"
 }
 
+variable "ec2_instance_type" {
+  description = "EC2 instance type to launch"
+}
+
+variable "keypair_name" {
+  description = "AWS Key pair used to connect to EC2 instances"
+  default = "scheduler"
+}
 
